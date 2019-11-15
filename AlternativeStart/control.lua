c35e5f5ba9f1f9731bd3c960dc83faeb2ce0cde6
Vas <VasVadum@Gmail.com>
2019-11-15 03:58:18 -0600

    Version 0.5.1

--- AlternativeStart/control.lua
+++ AlternativeStart/control.lua
@@ -1,7 +1,7 @@
 --[[
-	Code proudly stolen from AyrA!
-	Then tweaked to make my own little thing here. ~Vas
-	If you use this, please make sure to mention us somewhere like in your readme.  "Code proudly stolen from Vas which was proudly stolen from AyrA!" xP
+	Code originally referenced from AyrA.
+	Modified greatly and improved a lot by Vas.
+	If you use this, please make sure to mention us somewhere like in your readme.
 ]]--
 
 -- User Settings
@@ -15,39 +15,44 @@ local ADD_ROBOTS=		settings.global["basic-kit-robot"].value
 local ADD_ROCHEST=		settings.global["basic-kit-robotchests"].value
 local ADD_CAR=			settings.global["basic-kit-car"].value
 local PERFECTIONIST=	settings.global["basic-kit-perfectionist"].value
-local ADD_MONEY=		settings.global["basic-kit-money"].value
+local MODU_ROBO=		settings.global["basic-kit-modularrobots"].value
+local START_EQUIP=			settings.global["basic-kit-start-string"].value
+local START_TECH=			settings.global["basic-kit-start-research-string"].value
+local DISASABLE_EQUIP=		settings.global["basic-kit-disable-equipment-string"].value
+local DISABLE_TECH=			settings.global["basic-kit-disable-research-string"].value
 
 --Start the kit table.
 local kit={
-	{name="iron-plate",count=4},
-	{name="iron-axe",count=1}
+	{name="iron-plate",count=4}
 }
-local MY_MONEY = 50
 -- Add stuff to inventory depending on the settings
---[[if ADD_YOUR_OWN then
-	
-else
-end]]--
 
+-- Lets create a splitter function so we can split our additional strings properly.
+local function split(inputstr, sep)
+	if sep == nil then sep = "%s" end
+	t={} i=1
+	for str in string.gmatch(inputstr, "([^"..sep.."]+)") do 
+		t[i] = str
+		i = i + 1
+	end return t
+end
+
+--[[Choose Mining Equipment]]--
 if BASE_KIT == "Base" then
 	table.insert(kit,{name="burner-mining-drill",count=1})
 	table.insert(kit,{name="stone-furnace",count=1})
-	MY_MONEY = MY_MONEY + 50
 elseif BASE_KIT == "Electric" then
 	table.insert(kit,{name="electric-mining-drill",count=1})
 	table.insert(kit,{name="medium-electric-pole",count=1})
 	table.insert(kit,{name="stone-furnace",count=1})
-	MY_MONEY = MY_MONEY +25
 elseif BASE_KIT == "Easy Electric" then
 	table.insert(kit,{name="electric-mining-drill",count=2})
 	table.insert(kit,{name="medium-electric-pole",count=5})
 	table.insert(kit,{name="stone-furnace",count=3})
-	MY_MONEY = MY_MONEY + 5
 elseif BASE_KIT == "Mega Electric" then
 	table.insert(kit,{name="electric-mining-drill",count=4})
 	table.insert(kit,{name="medium-electric-pole",count=20})
 	table.insert(kit,{name="stone-furnace",count=6})
-	MY_MONEY = MY_MONEY - 25
 elseif BASE_KIT == "Epic Electric" then
 	table.insert(kit,{name="electric-mining-drill",count=8})
 	table.insert(kit,{name="electric-furnace",count=10})
@@ -56,24 +61,17 @@ elseif BASE_KIT == "Epic Electric" then
 	else
 		table.insert(kit,{name="substation",count=4})
 	end
-	MY_MONEY = MY_MONEY - 60
-else
-	table.insert(kit,{name="burner-mining-drill",count=1})
-	table.insert(kit,{name="stone-furnace",count=1})
-	MY_MONEY = MY_MONEY + 50
 end
 
--- Equip the player with weapons and ammo, eventually I will even disable research for handheld weapons once I figure out how for an "else" version.
+--[[Choose your combat starting gear]]--
 if EQUIPME == "Civilian" then
 	table.insert(kit,{name="pistol",count=1})
 	table.insert(kit,{name="firearm-magazine",count=10})
-	MY_MONEY = MY_MONEY + 65
 elseif EQUIPME == "Soldier" then
 	table.insert(kit,{name="submachine-gun",count=1})
 	table.insert(kit,{name="shotgun",count=1})
 	table.insert(kit,{name="piercing-rounds-magazine",count=20})
 	table.insert(kit,{name="piercing-shotgun-shell",count=20})
-	MY_MONEY = MY_MONEY + 10
 elseif EQUIPME == "Rambo" then
 	table.insert(kit,{name="submachine-gun",count=1})
 	table.insert(kit,{name="flamethrower",count=1})
@@ -81,17 +79,14 @@ elseif EQUIPME == "Rambo" then
 	table.insert(kit,{name="uranium-rounds-magazine",count=100})
 	table.insert(kit,{name="flamethrower-ammo",count=50})
 	table.insert(kit,{name="piercing-shotgun-shell",count=50})
-	MY_MONEY = MY_MONEY - 75
-else
-	MY_MONEY = MY_MONEY + 100
 end
 
+--[[Steam or Solar start]]--
 if POWER_KIT == "Steam" then
 	table.insert(kit,{name="offshore-pump",count=1})
 	table.insert(kit,{name="boiler",count=1})
 	table.insert(kit,{name="pipe",count=10})
 	table.insert(kit,{name="steam-engine",count=1})
-	MY_MONEY = MY_MONEY + 15
 elseif POWER_KIT == "Solar" then
 	table.insert(kit,{name="solar-panel",count=6})
 	table.insert(kit,{name="accumulator",count=3})
@@ -100,7 +95,6 @@ elseif POWER_KIT == "Solar" then
 	else
 		table.insert(kit,{name="substation",count=2})
 	end
-	MY_MONEY = MY_MONEY - 5
 elseif POWER_KIT == "Super Solar" then
 	table.insert(kit,{name="solar-panel",count=12})
 	table.insert(kit,{name="accumulator",count=6})
@@ -109,7 +103,6 @@ elseif POWER_KIT == "Super Solar" then
 	else
 		table.insert(kit,{name="substation",count=4})
 	end
-	MY_MONEY = MY_MONEY - 20
 elseif POWER_KIT == "Insane Solar" then
 	table.insert(kit,{name="solar-panel",count=24})
 	table.insert(kit,{name="accumulator",count=12})
@@ -118,15 +111,12 @@ elseif POWER_KIT == "Insane Solar" then
 	else
 		table.insert(kit,{name="substation",count=8})
 	end
-	MY_MONEY = MY_MONEY - 60
-else
-	MY_MONEY = MY_MONEY + 100
 end
 
+--[[Easier Start]]--
 if ADD_EASY == "Resources" then
 	table.insert(kit,{name="iron-plate",count=46})
 	table.insert(kit,{name="copper-plate",count=50})
-	MY_MONEY = MY_MONEY - 10
 elseif ADD_EASY == "Power Armor" then
 	table.insert(kit,{name="modular-armor",count=1})
 	table.insert(kit,{name="energy-shield-equipment",count=1})
@@ -134,13 +124,15 @@ elseif ADD_EASY == "Power Armor" then
 	table.insert(kit,{name="battery-equipment",count=1})
 	table.insert(kit,{name="solar-panel-equipment",count=7})
 	table.insert(kit,{name="exoskeleton-equipment",count=1})
-	MY_MONEY = MY_MONEY - 20
 elseif ADD_EASY == "Research Equipment" then
-	table.insert(kit,{name="lab",count=1})
-	table.insert(kit,{name="science-pack-1",count=50})
-	table.insert(kit,{name="science-pack-2",count=50})
-	table.insert(kit,{name="science-pack-3",count=50})
-	MY_MONEY = MY_MONEY - 40
+	if game.active_mods["timed_technology"] then
+		table.insert(kit,{name="lab",count=1})
+	else
+		table.insert(kit,{name="lab",count=1})
+		table.insert(kit,{name="automation-science-pack",count=50}) --science-pack-1
+		table.insert(kit,{name="logistic-science-pack",count=50}) --science-pack-2
+		table.insert(kit,{name="chemical-science-pack",count=50}) --science-pack-3
+	end
 elseif ADD_EASY == "Power Armor & Resources" then
 	table.insert(kit,{name="iron-plate",count=46})
 	table.insert(kit,{name="copper-plate",count=50})
@@ -150,7 +142,6 @@ elseif ADD_EASY == "Power Armor & Resources" then
 	table.insert(kit,{name="battery-equipment",count=1})
 	table.insert(kit,{name="solar-panel-equipment",count=7})
 	table.insert(kit,{name="exoskeleton-equipment",count=1})
-	MY_MONEY = MY_MONEY - 30
 elseif ADD_EASY == "All" then
 	table.insert(kit,{name="iron-plate",count=46})
 	table.insert(kit,{name="copper-plate",count=50})
@@ -161,91 +152,105 @@ elseif ADD_EASY == "All" then
 	table.insert(kit,{name="solar-panel-equipment",count=7})
 	table.insert(kit,{name="exoskeleton-equipment",count=1})
 	table.insert(kit,{name="lab",count=1})
-	table.insert(kit,{name="science-pack-1",count=50})
-	table.insert(kit,{name="science-pack-2",count=50})
-	table.insert(kit,{name="science-pack-3",count=50})
-	MY_MONEY = MY_MONEY - 70
+	table.insert(kit,{name="automation-science-pack",count=50}) --science-pack-1
+	table.insert(kit,{name="logistic-science-pack",count=50}) --science-pack-2
+	table.insert(kit,{name="chemical-science-pack",count=50}) --science-pack-3
 end
 
+--[[Modules]]--
 if ADD_MODULES == "Speed" then
 	table.insert(kit,{name="speed-module-3",count=4})
-	MY_MONEY = MY_MONEY - 10
 elseif ADD_MODULES == "Productivity" then
 	table.insert(kit,{name="productivity-module-3",count=4})
-	MY_MONEY = MY_MONEY - 10
 elseif ADD_MODULES == "Energy Efficiency" then
 	table.insert(kit,{name="effectivity-module-3",count=4})
-	MY_MONEY = MY_MONEY - 10
 elseif ADD_MODULES == "All" then
 	table.insert(kit,{name="speed-module-3",count=4})
 	table.insert(kit,{name="productivity-module-3",count=4})
 	table.insert(kit,{name="effectivity-module-3",count=4})
-	MY_MONEY = MY_MONEY - 30
 elseif ADD_MODULES == "4x All" then
 	table.insert(kit,{name="speed-module-3",count=16})
 	table.insert(kit,{name="productivity-module-3",count=16})
 	table.insert(kit,{name="effectivity-module-3",count=16})
-	MY_MONEY = MY_MONEY - 120
-else
-	MY_MONEY = MY_MONEY + 40
 end
 
+--[[Start with bots]]--
 if ADD_ROBOTS == "Basic" then
-	table.insert(kit,{name="roboport",count=1})
+	if MODU_ROBO then --[[settings.global["bobmods-logistics-disableroboports"].value == true OR game.active_mods["boblogistics"]]--
+		table.insert(kit,{name="bob-robochest",count=1})
+		table.insert(kit,{name="bob-robo-charge-port",count=1})
+		table.insert(kit,{name="bob-logistic-zone-interface",count=1})
+		table.insert(kit,{name="bob-logistic-zone-expander",count=1})
+	else
+		table.insert(kit,{name="roboport",count=1})
+	end
 	table.insert(kit,{name="logistic-robot",count=10})
 	table.insert(kit,{name="construction-robot",count=2})
-	MY_MONEY = MY_MONEY - 20
 	if ADD_ROCHEST then
 		table.insert(kit,{name="logistic-chest-passive-provider",count=3})
 		table.insert(kit,{name="logistic-chest-storage",count=1})
 		table.insert(kit,{name="logistic-chest-requester",count=3})
-	MY_MONEY = MY_MONEY - 10
 	end
 elseif ADD_ROBOTS == "Lots" then
-	table.insert(kit,{name="roboport",count=1})
+	if MODU_ROBO then
+		table.insert(kit,{name="bob-robochest",count=1})
+		table.insert(kit,{name="bob-robo-charge-port",count=2})
+		table.insert(kit,{name="bob-logistic-zone-interface",count=1})
+		table.insert(kit,{name="bob-logistic-zone-expander-2",count=1})
+	else
+		table.insert(kit,{name="roboport",count=1})
+	end
 	table.insert(kit,{name="logistic-robot",count=25})
 	table.insert(kit,{name="construction-robot",count=5})
-	MY_MONEY = MY_MONEY - 35
 	if ADD_ROCHEST then
 		table.insert(kit,{name="logistic-chest-passive-provider",count=6})
 		table.insert(kit,{name="logistic-chest-storage",count=3})
 		table.insert(kit,{name="logistic-chest-requester",count=6})
-	MY_MONEY = MY_MONEY - 20
 	end
 elseif ADD_ROBOTS == "Too Many" then
-	table.insert(kit,{name="roboport",count=2})
+	if MODU_ROBO then
+		table.insert(kit,{name="bob-robochest",count=1})
+		table.insert(kit,{name="bob-robo-charge-port",count=3})
+		table.insert(kit,{name="bob-logistic-zone-interface",count=1})
+		table.insert(kit,{name="bob-logistic-zone-expander-3",count=2})
+	else
+		table.insert(kit,{name="roboport",count=2})
+	end
 	table.insert(kit,{name="logistic-robot",count=50})
 	table.insert(kit,{name="construction-robot",count=10})
-	MY_MONEY = MY_MONEY - 60
 	if ADD_ROCHEST then
 		table.insert(kit,{name="logistic-chest-passive-provider",count=10})
 		table.insert(kit,{name="logistic-chest-storage",count=5})
 		table.insert(kit,{name="logistic-chest-requester",count=10})
-	MY_MONEY = MY_MONEY - 30
 	end
 elseif ADD_ROBOTS == "Too Many ++" then
-	table.insert(kit,{name="roboport",count=4})
+	if MODU_ROBO then
+		table.insert(kit,{name="bob-robochest",count=2})
+		table.insert(kit,{name="bob-robo-charge-port",count=4})
+		table.insert(kit,{name="bob-robo-charge-port-large",count=1})
+		table.insert(kit,{name="bob-logistic-zone-interface",count=1})
+		table.insert(kit,{name="bob-logistic-zone-expander-4",count=2})
+	else
+		table.insert(kit,{name="roboport",count=4})
+	end
 	table.insert(kit,{name="logistic-robot",count=100})
 	table.insert(kit,{name="construction-robot",count=20})
-	MY_MONEY = MY_MONEY - 100
 	if ADD_ROCHEST then
 		table.insert(kit,{name="logistic-chest-passive-provider",count=15})
 		table.insert(kit,{name="logistic-chest-storage",count=10})
 		table.insert(kit,{name="logistic-chest-requester",count=15})
 		table.insert(kit,{name="logistic-chest-active-provider",count=5})
 		table.insert(kit,{name="logistic-chest-buffer",count=5})
-	MY_MONEY = MY_MONEY - 45
 	end
 end
 
+--[[Add a car]]--
 if ADD_CAR == "Car" then
 	table.insert(kit,{name="car",count=1})
 	table.insert(kit,{name="solid-fuel",count=5})
-	MY_MONEY = MY_MONEY - 5
 elseif ADD_CAR == "Tank" then
 	table.insert(kit,{name="tank",count=1})
 	table.insert(kit,{name="solid-fuel",count=5})
-	MY_MONEY = MY_MONEY - 20
 elseif ADD_CAR == "Train" then
 	table.insert(kit,{name="locomotive",count=1})
 	table.insert(kit,{name="cargo-wagon",count=2})
@@ -255,27 +260,43 @@ elseif ADD_CAR == "Train" then
 	table.insert(kit,{name="train-stop",count=2})
 	table.insert(kit,{name="rail-signal",count=20})
 	table.insert(kit,{name="rail-chain-signal",count=20})
-	MY_MONEY = MY_MONEY - 60
 end
 
 if PERFECTIONIST > 0 then
 	table.insert(kit,{name="landfill",count=PERFECTIONIST})
-	MY_MONEY = MY_MONEY - math.floor((PERFECTIONIST / 2))
 end
 
-if ADD_MONEY then
-	if MY_MONEY >= 1 then
-		table.insert(kit,{name="coin",count=MY_MONEY})
-	end
+-- MODS
+if settings.startup["start-with-vehicle-miner"].value == true then
+	table.insert(kit,{name="vehicle-miner",count=1})
+end
+
+if settings.startup["start-with-unit-remote-control"].value == true then
+	table.insert(kit,{name="unit-remote-control",count=1})
 end
 
 -- Now lets get the player that just joined, and clear all of his inventory to replace with ours.
 script.on_event(defines.events.on_player_created,function(param)
 	local p=game.players[param.player_index]
-	if MY_MONEY <= 0 then
-		p.print("Sorry, your account balance is " .. MY_MONEY .. "FC, so we reposessed your house.")
-		p.print("Your spouse left you too and in your absence took everything you had left, and is now dating your best friend.")
+	
+	if START_EQUIP ~= "" then
+		local items = split(START_EQUIP,",")
+		for no, item_str in pairs(items) do
+			local info = split(item_str,":")
+			if game.item_prototypes[info[1]] then
+				table.insert(kit, {name=info[1],count=tonumber(info[2])})
+			end
+		end
 	end
+
+	--[[if START_TECH == "everything" then
+		--p.force.research_all_technologies()
+	elseif ~= "" then
+		local techs = split(START_TECH,",")
+		for no, tech_str in pairs(techs) do
+			--p.force.technology[techs].enabled=true
+		end
+	end]]--
 
 	-- Here we run a check to clear all sections of his inventory.
 	for i,v in pairs(defines.inventory) do
--- AlternativeStart/settings.lua
+++ AlternativeStart/settings.lua
@@ -12,6 +12,172 @@ data:extend({
 		localised_description = "You can choose from base game, electric, 2x electric, or 4x electric.",
 		setting_type = "runtime-global",
 		default_value = "Base",
+		allowed_values = {"None","Base","Electric","Easy Electric","Mega Electric","Epic Electric"},
+		order = "a-a"
+	},
+	{
+		-- Choose what type of military equipment you start with.
+		type = "string-setting",
+		name = "basic-kit-equipment",
+		localised_name = "Choose your combat starting gear",
+		localised_description = "You can choose your combat equipment here.",
+		setting_type = "runtime-global",
+		default_value = "Civilian",
+		allowed_values = {"None","Civilian","Soldier","Rambo"},
+		order = "b-a"
+	},
+	{
+		-- Steam or Solar start
+		type = "string-setting",
+		name = "basic-kit-power",
+		localised_name = "Choose your starting power methods",
+		localised_description = "Choose whether to start with steam power or solar power or even improved solar.",
+		setting_type = "runtime-global",
+		default_value = "None",
+		allowed_values = {"None","Steam","Solar","Super Solar","Insane Solar"},
+		order = "c-a"
+	},
+	{
+		-- 4x the substations
+		type = "bool-setting",
+		name = "basic-kit-moresubs",
+		localised_name = "More Substations?",
+		localised_description = "Should we multiply the amount of substations you get by 4? (Only works with options that give substations)",
+		setting_type = "runtime-global",
+		default_value = false,
+		order = "d-a"
+	},
+	{
+		-- Easy start boost
+		type = "string-setting",
+		name = "basic-kit-easy",
+		localised_name = "Easier Start",
+		localised_description = "Choose which type of easy start you will get.",
+		setting_type = "runtime-global",
+		default_value = "Nope",
+		allowed_values = {"Nope","Resources","Power Armor","Research Equipment","Power Armor & Resources","All"},
+		order = "e-a"
+	},
+	{
+		-- Modules
+		type = "string-setting",
+		name = "basic-kit-module",
+		localised_name = "Do you wish to bring some modules?",
+		localised_description = "Choose which type of modules you brought with you. The options are fairly obvious.",
+		setting_type = "runtime-global",
+		default_value = "None",
+		allowed_values = {"None","Speed","Productivity","Energy Efficiency","All","4x All"},
+		order = "e-a"
+	},
+	{
+		-- Start with bots
+		type = "string-setting",
+		name = "basic-kit-robot",
+		localised_name = "Robots",
+		localised_description = "Would you like to start with bots?",
+		setting_type = "runtime-global",
+		default_value = "Nope",
+		allowed_values = {"Nope","Basic","Lots","Too Many","Too Many ++"},
+		order = "f-a"
+	},
+	{
+		-- Switch robo ports to modular
+		type = "bool-setting",
+		name = "basic-kit-modularrobots",
+		localised_name = "Modular Robot Ports?",
+		localised_description = "Would you like to have modular robot ports rather than the standard robo port?",
+		setting_type = "runtime-global",
+		default_value = false,
+		order = "f-b"
+	},
+	{
+		-- Add chests with your robots
+		type = "bool-setting",
+		name = "basic-kit-robotchests",
+		localised_name = "Logistic Chests?",
+		localised_description = "Would you like to add robot logistical chests to your robot selection?",
+		setting_type = "runtime-global",
+		default_value = false,
+		order = "f-c"
+	},
+	{
+		-- Add a car
+		type = "string-setting",
+		name = "basic-kit-car",
+		localised_name = "Choose a starting vehicle.",
+		localised_description = "Each vehicle also comes with a little bit of solid fuel. Train option is currently beta, please let me know if I should alter it any.",
+		setting_type = "runtime-global",
+		default_value = "None",
+		allowed_values = {"None","Car","Tank","Train"},
+		order = "h-a"
+	},
+	{
+		-- Start with landfill
+		type = "int-setting",
+		name = "basic-kit-perfectionist",
+		localised_name = "Perfectionist",
+		localised_description = "Are you a man who dislikes water where you don't want it? Did you decide to bring some landfill with you to handle messy water ways? If so, how much did you bring? (1000 is the max)",
+		setting_type = "runtime-global",
+		default_value = 0,
+		maximum_value = 1000,
+		minimum_value = 0,
+		order = "i-a"
+	},
+	{
+		-- Choose the exact equipment you wish to start with.
+		type = "string-setting",
+		name = "basic-kit-start-string",
+		localised_name = "Choose Extra Equipment",
+		localised_description = "Here, you can choose the equipment you would like to start with separating them out with commas and giving an amount you wish to have. Referrer to the example given in default value. You can also get some preset values from the mod's upload page.",
+		setting_type = "runtime-global",
+		allow_blank = true,
+		default_value = "iron-plate:4,iron-axe:1,burner-mining-drill:1,stone-furnace:1,pistol:1,firearm-magazine:10",
+		order = "k-a"
+	},
+	{
+		-- Choose the exact research you wish to start with.
+		type = "string-setting",
+		name = "basic-kit-start-research-string",
+		localised_name = "Choose Starting Technologies",
+		localised_description = "Here you will choose the technology you start with, separating each one with a comma. It is possible to start with a technology without its pre-reqs.",
+		setting_type = "runtime-global",
+		allow_blank = true,
+		default_value = "DOES NOT WORK YET",
+		order = "k-b"
+	},
+	{
+		-- Choose the exact equipment you wish to disable.
+		type = "string-setting",
+		name = "basic-kit-disable-equipment-string",
+		localised_name = "Disable Specific Items",
+		localised_description = "Here, you can disable specific items like the burner inserter, or other recipes so that they can't be made and won't show up.",
+		setting_type = "runtime-global",
+		allow_blank = true,
+		default_value = "DOES NOT WORK YET",
+		order = "k-c"
+	},
+	{
+		-- Choose the exact research you wish to disable.
+		type = "string-setting",
+		name = "basic-kit-disable-research-string",
+		localised_name = "Disable Specific Technologies",
+		localised_description = "Here, you can disable specific technologies so that they can't be unlocked, and won't show up.",
+		setting_type = "runtime-global",
+		allow_blank = true,
+		default_value = "DOES NOT WORK YET",
+		order = "k-d"
+	}
+})
+
+--[[data:extend({
+	{
+		-- Choose what type of mining equipment you start with.
+		type = "string-setting",
+		name = "basic-kit-base",
+		localised_name = "Choose Mining Equipment",
+		localised_description = "You can choose from base game, electric, 2x electric, or 4x electric.",
+		setting_type = "runtime-global",
+		default_value = "Base",
 		allowed_values = {"Base","Electric","Easy Electric","Mega Electric","Epic Electric"},
 		order = "a-a"
 	},
@@ -24,7 +190,7 @@ data:extend({
 		setting_type = "runtime-global",
 		default_value = "Civilian",
 		allowed_values = {"None","Civilian","Soldier","Rambo"},
-		order = "b-b"
+		order = "b-a"
 	},
 	{
 		-- Steam or Solar start
@@ -35,7 +201,7 @@ data:extend({
 		setting_type = "runtime-global",
 		default_value = "None",
 		allowed_values = {"None","Steam","Solar","Super Solar","Insane Solar"},
-		order = "c-c"
+		order = "c-a"
 	},
 	{
 		-- 4x the substations
@@ -45,7 +211,7 @@ data:extend({
 		localised_description = "Should we multiply the amount of substations you get by 4? (Only works with options that give substations)",
 		setting_type = "runtime-global",
 		default_value = false,
-		order = "d-d"
+		order = "d-a"
 	},
 	{
 		-- Easy start boost
@@ -56,7 +222,7 @@ data:extend({
 		setting_type = "runtime-global",
 		default_value = "Nope",
 		allowed_values = {"Nope","Resources","Power Armor","Research Equipment","Power Armor & Resources","All"},
-		order = "e-e"
+		order = "e-a"
 	},
 	{
 		-- Modules
@@ -67,7 +233,7 @@ data:extend({
 		setting_type = "runtime-global",
 		default_value = "None",
 		allowed_values = {"None","Speed","Productivity","Energy Efficiency","All","4x All"},
-		order = "e-f"
+		order = "e-a"
 	},
 	{
 		-- Start with bots
@@ -78,7 +244,7 @@ data:extend({
 		setting_type = "runtime-global",
 		default_value = "Nope",
 		allowed_values = {"Nope","Basic","Lots","Too Many","Too Many ++"},
-		order = "f-f"
+		order = "f-a"
 	},
 	{
 		-- Add chests with your robots
@@ -88,7 +254,7 @@ data:extend({
 		localised_description = "Would you like to add robot logistical chests to your robot selection?",
 		setting_type = "runtime-global",
 		default_value = false,
-		order = "g-g"
+		order = "g-a"
 	},
 	{
 		-- Add a car
@@ -99,7 +265,7 @@ data:extend({
 		setting_type = "runtime-global",
 		default_value = "None",
 		allowed_values = {"None","Car","Tank","Train"},
-		order = "h-h"
+		order = "h-a"
 	},
 	{
 		-- Start with landfill
@@ -111,16 +277,6 @@ data:extend({
 		default_value = 0,
 		maximum_value = 1000,
 		minimum_value = 0,
-		order = "i-i"
-	},
-	{
-		-- Useless money
-		type = "bool-setting",
-		name = "basic-kit-money",
-		localised_name = "Bring Money",
-		localised_description = "So, you're so paranoid that you actually decided to bring your money with you even though its useless here!",
-		setting_type = "runtime-global",
-		default_value = false,
-		order = "j-j"
+		order = "i-a"
 	}
-})
+})]]--
