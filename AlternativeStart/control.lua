16c4a1fdbceeac14a3c0529d2bedf4f4c78cefdc
David-John Miller <anoyomouse@gmail.com>
2021-02-08 16:40:44 +0200

    Fixed for freeplay

--- AlternativeStart/control.lua
+++ AlternativeStart/control.lua
@@ -39,243 +39,250 @@ end
 
 --[[Choose Mining Equipment]]--
 if BASE_KIT == "Base" then
-	table.insert(kit,{name="burner-mining-drill",count=1})
-	table.insert(kit,{name="stone-furnace",count=1})
+	kit["burner-mining-drill"]=1
+	kit["stone-furnace"]=1
 elseif BASE_KIT == "Electric" then
-	table.insert(kit,{name="electric-mining-drill",count=1})
-	table.insert(kit,{name="medium-electric-pole",count=1})
-	table.insert(kit,{name="stone-furnace",count=1})
+	kit["electric-mining-drill"]=1
+	kit["medium-electric-pole"]=1
+	kit["stone-furnace"]=1
 elseif BASE_KIT == "Easy Electric" then
-	table.insert(kit,{name="electric-mining-drill",count=2})
-	table.insert(kit,{name="medium-electric-pole",count=5})
-	table.insert(kit,{name="stone-furnace",count=3})
+	kit["electric-mining-drill"]=2
+	kit["medium-electric-pole"]=5
+	kit["stone-furnace"]=3
 elseif BASE_KIT == "Mega Electric" then
-	table.insert(kit,{name="electric-mining-drill",count=4})
-	table.insert(kit,{name="medium-electric-pole",count=20})
-	table.insert(kit,{name="stone-furnace",count=6})
+	kit["electric-mining-drill"]=4
+	kit["medium-electric-pole"]=20
+	kit["stone-furnace"]=6
 elseif BASE_KIT == "Epic Electric" then
-	table.insert(kit,{name="electric-mining-drill",count=8})
-	table.insert(kit,{name="electric-furnace",count=10})
+	kit["electric-mining-drill"]=8
+	kit["electric-furnace"]=10
 	if MORE_SUBS then
-		table.insert(kit,{name="substation",count=16})
+		kit["substation"]=16
 	else
-		table.insert(kit,{name="substation",count=4})
+		kit["substation"]=4
 	end
 end
 
 --[[Choose your combat starting gear]]--
 if EQUIPME == "Civilian" then
-	table.insert(kit,{name="pistol",count=1})
-	table.insert(kit,{name="firearm-magazine",count=10})
+	kit["pistol"]=1
+	kit["firearm-magazine"]=10
 elseif EQUIPME == "Soldier" then
-	table.insert(kit,{name="submachine-gun",count=1})
-	table.insert(kit,{name="shotgun",count=1})
-	table.insert(kit,{name="piercing-rounds-magazine",count=20})
-	table.insert(kit,{name="piercing-shotgun-shell",count=20})
+	kit["submachine-gun"]=1
+	kit["shotgun"]=1
+	kit["piercing-rounds-magazine"]=20
+	kit["piercing-shotgun-shell"]=20
 elseif EQUIPME == "Rambo" then
-	table.insert(kit,{name="submachine-gun",count=1})
-	table.insert(kit,{name="flamethrower",count=1})
-	table.insert(kit,{name="combat-shotgun",count=1})
-	table.insert(kit,{name="uranium-rounds-magazine",count=100})
-	table.insert(kit,{name="flamethrower-ammo",count=50})
-	table.insert(kit,{name="piercing-shotgun-shell",count=50})
+	kit["submachine-gun"]=1
+	kit["flamethrower"]=1
+	kit["combat-shotgun"]=1
+	kit["uranium-rounds-magazine"]=100
+	kit["flamethrower-ammo"]=50
+	kit["piercing-shotgun-shell"]=50
 end
 
 --[[Steam or Solar start]]--
 if POWER_KIT == "Steam" then
-	table.insert(kit,{name="offshore-pump",count=1})
-	table.insert(kit,{name="boiler",count=1})
-	table.insert(kit,{name="pipe",count=10})
-	table.insert(kit,{name="steam-engine",count=1})
+	kit["offshore-pump"]=1
+	kit["boiler"]=1
+	kit["pipe"]=10
+	kit["steam-engine"]=1
 elseif POWER_KIT == "Solar" then
-	table.insert(kit,{name="solar-panel",count=6})
-	table.insert(kit,{name="accumulator",count=3})
+	kit["solar-panel"]=6
+	kit["accumulator"]=3
 	if MORE_SUBS then
-		table.insert(kit,{name="substation",count=8})
+		kit["substation"]=8
 	else
-		table.insert(kit,{name="substation",count=2})
+		kit["substation"]=2
 	end
 elseif POWER_KIT == "Super Solar" then
-	table.insert(kit,{name="solar-panel",count=12})
-	table.insert(kit,{name="accumulator",count=6})
+	kit["solar-panel"]=12
+	kit["accumulator"]=6
 	if MORE_SUBS then
-		table.insert(kit,{name="substation",count=16})
+		kit["substation"]=16
 	else
-		table.insert(kit,{name="substation",count=4})
+		kit["substation"]=4
 	end
 elseif POWER_KIT == "Insane Solar" then
-	table.insert(kit,{name="solar-panel",count=24})
-	table.insert(kit,{name="accumulator",count=12})
+	kit["solar-panel"]=24
+	kit["accumulator"]=12
 	if MORE_SUBS then
-		table.insert(kit,{name="substation",count=32})
+		kit["substation"]=32
 	else
-		table.insert(kit,{name="substation",count=8})
+		kit["substation"]=8
 	end
 end
 
 --[[Easier Start]]--
 if ADD_EASY == "Resources" then
-	table.insert(kit,{name="iron-plate",count=46})
-	table.insert(kit,{name="copper-plate",count=50})
+	kit["iron-plate"]=46
+	kit["copper-plate"]=50
 elseif ADD_EASY == "Power Armor" then
-	table.insert(kit,{name="modular-armor",count=1})
-	table.insert(kit,{name="energy-shield-equipment",count=1})
-	table.insert(kit,{name="night-vision-equipment",count=1})
-	table.insert(kit,{name="battery-equipment",count=1})
-	table.insert(kit,{name="solar-panel-equipment",count=7})
-	table.insert(kit,{name="exoskeleton-equipment",count=1})
+	kit["modular-armor"]=1
+	kit["energy-shield-equipment"]=1
+	kit["night-vision-equipment"]=1
+	kit["battery-equipment"]=1
+	kit["solar-panel-equipment"]=7
+	kit["exoskeleton-equipment"]=1
 elseif ADD_EASY == "Research Equipment" then
 	if game.active_mods["timed_technology"] then
-		table.insert(kit,{name="lab",count=1})
+		kit["lab"]=1
 	else
-		table.insert(kit,{name="lab",count=1})
-		table.insert(kit,{name="automation-science-pack",count=50}) --science-pack-1
-		table.insert(kit,{name="logistic-science-pack",count=50}) --science-pack-2
-		table.insert(kit,{name="chemical-science-pack",count=50}) --science-pack-3
+		kit["lab"]=1
+		kit["automation-science-pack"]=50 --science-pack-1
+		kit["logistic-science-pack"]=50 --science-pack-2
+		kit["chemical-science-pack"]=50 --science-pack-3
 	end
 elseif ADD_EASY == "Power Armor & Resources" then
-	table.insert(kit,{name="iron-plate",count=46})
-	table.insert(kit,{name="copper-plate",count=50})
-	table.insert(kit,{name="modular-armor",count=1})
-	table.insert(kit,{name="energy-shield-equipment",count=1})
-	table.insert(kit,{name="night-vision-equipment",count=1})
-	table.insert(kit,{name="battery-equipment",count=1})
-	table.insert(kit,{name="solar-panel-equipment",count=7})
-	table.insert(kit,{name="exoskeleton-equipment",count=1})
+	kit["iron-plate"]=46
+	kit["copper-plate"]=50
+	kit["modular-armor"]=1
+	kit["energy-shield-equipment"]=1
+	kit["night-vision-equipment"]=1
+	kit["battery-equipment"]=1
+	kit["solar-panel-equipment"]=7
+	kit["exoskeleton-equipment"]=1
 elseif ADD_EASY == "All" then
-	table.insert(kit,{name="iron-plate",count=46})
-	table.insert(kit,{name="copper-plate",count=50})
-	table.insert(kit,{name="modular-armor",count=1})
-	table.insert(kit,{name="energy-shield-equipment",count=1})
-	table.insert(kit,{name="night-vision-equipment",count=1})
-	table.insert(kit,{name="battery-equipment",count=1})
-	table.insert(kit,{name="solar-panel-equipment",count=7})
-	table.insert(kit,{name="exoskeleton-equipment",count=1})
-	table.insert(kit,{name="lab",count=1})
-	table.insert(kit,{name="automation-science-pack",count=50}) --science-pack-1
-	table.insert(kit,{name="logistic-science-pack",count=50}) --science-pack-2
-	table.insert(kit,{name="chemical-science-pack",count=50}) --science-pack-3
+	kit["iron-plate"]=46
+	kit["copper-plate"]=50
+	kit["modular-armor"]=1
+	kit["energy-shield-equipment"]=1
+	kit["night-vision-equipment"]=1
+	kit["battery-equipment"]=1
+	kit["solar-panel-equipment"]=7
+	kit["exoskeleton-equipment"]=1
+	kit["lab"]=1
+	kit["automation-science-pack"]=50 --science-pack-1
+	kit["logistic-science-pack"]=50 --science-pack-2
+	kit["chemical-science-pack"]=50 --science-pack-3
 end
 
 --[[Modules]]--
 if ADD_MODULES == "Speed" then
-	table.insert(kit,{name="speed-module-3",count=4})
+	kit["speed-module-3"]=4
 elseif ADD_MODULES == "Productivity" then
-	table.insert(kit,{name="productivity-module-3",count=4})
+	kit["productivity-module-3"]=4
 elseif ADD_MODULES == "Energy Efficiency" then
-	table.insert(kit,{name="effectivity-module-3",count=4})
+	kit["effectivity-module-3"]=4
 elseif ADD_MODULES == "All" then
-	table.insert(kit,{name="speed-module-3",count=4})
-	table.insert(kit,{name="productivity-module-3",count=4})
-	table.insert(kit,{name="effectivity-module-3",count=4})
+	kit["speed-module-3"]=4
+	kit["productivity-module-3"]=4
+	kit["effectivity-module-3"]=4
 elseif ADD_MODULES == "4x All" then
-	table.insert(kit,{name="speed-module-3",count=16})
-	table.insert(kit,{name="productivity-module-3",count=16})
-	table.insert(kit,{name="effectivity-module-3",count=16})
+	kit["speed-module-3"]=16
+	kit["productivity-module-3"]=16
+	kit["effectivity-module-3"]=16
 end
 
 --[[Start with bots]]--
 if ADD_ROBOTS == "Basic" then
 	if MODU_ROBO then --[[settings.global["bobmods-logistics-disableroboports"].value == true OR game.active_mods["boblogistics"]]--
-		table.insert(kit,{name="bob-robochest",count=1})
-		table.insert(kit,{name="bob-robo-charge-port",count=1})
-		table.insert(kit,{name="bob-logistic-zone-interface",count=1})
-		table.insert(kit,{name="bob-logistic-zone-expander",count=1})
+		kit["bob-robochest"]=1
+		kit["bob-robo-charge-port"]=1
+		kit["bob-logistic-zone-interface"]=1
+		kit["bob-logistic-zone-expander"]=1
 	else
-		table.insert(kit,{name="roboport",count=1})
+		kit["roboport"]=1
 	end
-	table.insert(kit,{name="logistic-robot",count=10})
-	table.insert(kit,{name="construction-robot",count=2})
+	kit["logistic-robot"]=10
+	kit["construction-robot"]=2
 	if ADD_ROCHEST then
-		table.insert(kit,{name="logistic-chest-passive-provider",count=3})
-		table.insert(kit,{name="logistic-chest-storage",count=1})
-		table.insert(kit,{name="logistic-chest-requester",count=3})
+		kit["logistic-chest-passive-provider"]=3
+		kit["logistic-chest-storage"]=1
+		kit["logistic-chest-requester"]=3
 	end
 elseif ADD_ROBOTS == "Lots" then
 	if MODU_ROBO then
-		table.insert(kit,{name="bob-robochest",count=1})
-		table.insert(kit,{name="bob-robo-charge-port",count=2})
-		table.insert(kit,{name="bob-logistic-zone-interface",count=1})
-		table.insert(kit,{name="bob-logistic-zone-expander-2",count=1})
+		kit["bob-robochest"]=1
+		kit["bob-robo-charge-port"]=2
+		kit["bob-logistic-zone-interface"]=1
+		kit["bob-logistic-zone-expander-2"]=1
 	else
-		table.insert(kit,{name="roboport",count=1})
+		kit["roboport"]=1
 	end
-	table.insert(kit,{name="logistic-robot",count=25})
-	table.insert(kit,{name="construction-robot",count=5})
+	kit["logistic-robot"]=25
+	kit["construction-robot"]=5
 	if ADD_ROCHEST then
-		table.insert(kit,{name="logistic-chest-passive-provider",count=6})
-		table.insert(kit,{name="logistic-chest-storage",count=3})
-		table.insert(kit,{name="logistic-chest-requester",count=6})
+		kit["logistic-chest-passive-provider"]=6
+		kit["logistic-chest-storage"]=3
+		kit["logistic-chest-requester"]=6
 	end
 elseif ADD_ROBOTS == "Too Many" then
 	if MODU_ROBO then
-		table.insert(kit,{name="bob-robochest",count=1})
-		table.insert(kit,{name="bob-robo-charge-port",count=3})
-		table.insert(kit,{name="bob-logistic-zone-interface",count=1})
-		table.insert(kit,{name="bob-logistic-zone-expander-3",count=2})
+		kit["bob-robochest"]=1
+		kit["bob-robo-charge-port"]=3
+		kit["bob-logistic-zone-interface"]=1
+		kit["bob-logistic-zone-expander-3"]=2
 	else
-		table.insert(kit,{name="roboport",count=2})
+		kit["roboport"]=2
 	end
-	table.insert(kit,{name="logistic-robot",count=50})
-	table.insert(kit,{name="construction-robot",count=10})
+	kit["logistic-robot"]=50
+	kit["construction-robot"]=10
 	if ADD_ROCHEST then
-		table.insert(kit,{name="logistic-chest-passive-provider",count=10})
-		table.insert(kit,{name="logistic-chest-storage",count=5})
-		table.insert(kit,{name="logistic-chest-requester",count=10})
+		kit["logistic-chest-passive-provider"]=10
+		kit["logistic-chest-storage"]=5
+		kit["logistic-chest-requester"]=10
 	end
 elseif ADD_ROBOTS == "Too Many ++" then
 	if MODU_ROBO then
-		table.insert(kit,{name="bob-robochest",count=2})
-		table.insert(kit,{name="bob-robo-charge-port",count=4})
-		table.insert(kit,{name="bob-robo-charge-port-large",count=1})
-		table.insert(kit,{name="bob-logistic-zone-interface",count=1})
-		table.insert(kit,{name="bob-logistic-zone-expander-4",count=2})
+		kit["bob-robochest"]=2
+		kit["bob-robo-charge-port"]=4
+		kit["bob-robo-charge-port-large"]=1
+		kit["bob-logistic-zone-interface"]=1
+		kit["bob-logistic-zone-expander-4"]=2
 	else
-		table.insert(kit,{name="roboport",count=4})
+		kit["roboport"]=4
 	end
-	table.insert(kit,{name="logistic-robot",count=100})
-	table.insert(kit,{name="construction-robot",count=20})
+	kit["logistic-robot"]=100
+	kit["construction-robot"]=20
 	if ADD_ROCHEST then
-		table.insert(kit,{name="logistic-chest-passive-provider",count=15})
-		table.insert(kit,{name="logistic-chest-storage",count=10})
-		table.insert(kit,{name="logistic-chest-requester",count=15})
-		table.insert(kit,{name="logistic-chest-active-provider",count=5})
-		table.insert(kit,{name="logistic-chest-buffer",count=5})
+		kit["logistic-chest-passive-provider"]=15
+		kit["logistic-chest-storage"]=10
+		kit["logistic-chest-requester"]=15
+		kit["logistic-chest-active-provider"]=5
+		kit["logistic-chest-buffer"]=5
 	end
 end
 
 --[[Add a car]]--
 if ADD_CAR == "Car" then
-	table.insert(kit,{name="car",count=1})
-	table.insert(kit,{name="solid-fuel",count=5})
+	kit["car"]=1
+	kit["solid-fuel"]=5
 elseif ADD_CAR == "Tank" then
-	table.insert(kit,{name="tank",count=1})
-	table.insert(kit,{name="solid-fuel",count=5})
+	kit["tank"]=1
+	kit["solid-fuel"]=5
 elseif ADD_CAR == "Train" then
-	table.insert(kit,{name="locomotive",count=1})
-	table.insert(kit,{name="cargo-wagon",count=2})
-	table.insert(kit,{name="fluid-wagon",count=1})
-	table.insert(kit,{name="solid-fuel",count=50})
-	table.insert(kit,{name="rail",count=300})
-	table.insert(kit,{name="train-stop",count=2})
-	table.insert(kit,{name="rail-signal",count=20})
-	table.insert(kit,{name="rail-chain-signal",count=20})
+	kit["locomotive"]=1
+	kit["cargo-wagon"]=2
+	kit["fluid-wagon"]=1
+	kit["solid-fuel"]=50
+	kit["rail"]=300
+	kit["train-stop"]=2
+	kit["rail-signal"]=20
+	kit["rail-chain-signal"]=20
 end
 
 if PERFECTIONIST > 0 then
-	table.insert(kit,{name="landfill",count=PERFECTIONIST})
+	kit["landfill"]=PERFECTIONIST
 end
 
 -- MODS
 if settings.startup["start-with-vehicle-miner"].value == true then
-	table.insert(kit,{name="vehicle-miner",count=1})
+	kit["vehicle-miner"]=1
 end
 
 if settings.startup["start-with-unit-remote-control"].value == true then
-	table.insert(kit,{name="unit-remote-control",count=1})
+	kit["unit-remote-control"]=1
 end
 
+game.on_init(function(event)
+	if remote.interfaces["freeplay"]["set_created_items"] then
+		remote.call("freeplay", "set_created_items", kit);
+	end
+end);
+
 -- Now lets get the player that just joined, and clear all of his inventory to replace with ours.
+--[[
 script.on_event(defines.events.on_player_created,function(param)
 	local p=game.players[param.player_index]
 	
@@ -283,20 +290,20 @@ script.on_event(defines.events.on_player_created,function(param)
 		local items = split(START_EQUIP,",")
 		for no, item_str in pairs(items) do
 			local info = split(item_str,":")
-			if game.item_prototypes[info[1]] then
+			if game.item_prototypes[info[1] ] then
 				table.insert(kit, {name=info[1],count=tonumber(info[2])})
 			end
 		end
 	end
 
-	--[[if START_TECH == "everything" then
+	--[if START_TECH == "everything" then
 		--p.force.research_all_technologies()
 	elseif ~= "" then
 		local techs = split(START_TECH,",")
 		for no, tech_str in pairs(techs) do
 			--p.force.technology[techs].enabled=true
 		end
-	end]]--
+	end]--
 
 	-- Here we run a check to clear all sections of his inventory.
 	for i,v in pairs(defines.inventory) do
@@ -310,3 +317,4 @@ script.on_event(defines.events.on_player_created,function(param)
 		p.insert(v)
 	end
 end)
+]]--
